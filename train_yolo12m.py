56c5ac3caeb40cd476df49a6f1845328d91187bd
hungha1512 <hungha15123@gmail.com>
2025-12-15 07:58:07 +0700

    add train files

---
+++ train_yolo12m.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO11n model
+model = YOLO("ultralytics/ultralytics/cfg/models/12/yolo12m.yaml")
+
+# Train the model on the COCO8 dataset for 100 epochs
+train_results = model.train(
+    data="AGAR_lower_prepared/data.yaml",  # Path to dataset configuration file
+    epochs=80,  # Number of training epochs
+    imgsz=1024,  # Image size for training
+    pretrained=False,
+    batch=4
+)
+
+# Evaluate the model's performance on the validation set
+metrics = model.val()
